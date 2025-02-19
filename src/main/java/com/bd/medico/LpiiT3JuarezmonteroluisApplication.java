20d4928223cca7466f55e3f0d1cc6f021cb862e2
unknown <e2000juarez@gmail.com>
2025-02-19 15:15:18 -0500

    version inicio

---
+++ src/main/java/com/bd/medico/LpiiT3JuarezmonteroluisApplication.java
@@ -0,0 +1,13 @@
+package com.bd.medico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiT3JuarezmonteroluisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiT3JuarezmonteroluisApplication.class, args);
+	}
+
+}
